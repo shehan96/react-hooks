6677daf168a2e6818f1035c2a1acba7b3a845a3a
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-06-27 17:31:48 +0530

    Update StateSample.tsx

--- src/useState/StateSample.tsx
+++ src/useState/StateSample.tsx
@@ -1,16 +1,25 @@
 import { ChangeEvent, useState } from 'react';
 
 export const StateSample = () => {
+  // array destructuring with useState
   const [value, setValue] = useState('initial value');
+  const [count, setCount] = useState(0);
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+  };
+
+  const handleIncreaseCount = () => {
+    setCount(count + 1);
+    setCount((currentValue) => currentValue + 1);
   };
 
   return (
     <>
       <p>{value}</p>
       <input type='text' onChange={handleOnChange} />
+      <span>Count : {count}</span>
+      <button onClick={handleIncreaseCount}>Increase count</button>
     </>
   );
 };
