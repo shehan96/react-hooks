dd8aa34dce6d819d36c0aebca32382aee32b7caa
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-06-27 17:31:51 +0530

    Create UseReducer.tsx

---
+++ src/UseReducer/UseReducer.tsx
@@ -0,0 +1,30 @@
+import { useReducer } from 'react';
+
+type Action = {
+  type: 'increase' | 'decrise';
+  payload: number;
+};
+
+const reducer = (state: number, action : any) => {
+  switch (action.type) {
+    case 'increase':
+      return state + 1;
+    case 'decrise':
+      return state - 1;
+    default:
+      throw new Error('Undefine action type');
+  }
+};
+
+export const UseReducer = () => {
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <>
+      {state}
+      <h1>Use Reducer</h1>
+      <button onClick={() => dispatch({ type: 'increase', payload: 1 })}>+ Increase</button>
+      <button onClick={() => dispatch({ type: 'decrise', payload: 0 })}>- Descrease</button>
+    </>
+  );
+};
