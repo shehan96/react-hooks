21ab42580539dd915f70ad3708633602d25fb49c
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-06-28 17:38:49 +0530

    Create SampleCodes.tsx

---
+++ src/SampleCodes.tsx
@@ -0,0 +1,16 @@
+export const SampleCodes = () => {
+  // update single object is state array
+  /*
+  setTodos(todos.map(todo => {
+    if(t.id === todo.id){
+        t.completed = !t.completed;
+        return {...todo, t};
+    }
+    return todo;
+    }));
+    */
+  // add new item to the state array
+  /**
+      setNames([...names, newName]);
+     */
+};
