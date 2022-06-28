28bc67cb3b94501e8be67db1a1f0e91bbd406a83
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-06-28 12:15:00 +0530

    Update StateSample.tsx

--- src/useState/StateSample.tsx
+++ src/useState/StateSample.tsx
@@ -1,9 +1,15 @@
 import { ChangeEvent, useState } from 'react';
 
+type name = {
+  firstName: string;
+  lastName: string;
+};
+
 export const StateSample = () => {
   // array destructuring with useState
   const [value, setValue] = useState('initial value');
   const [count, setCount] = useState(0);
+  const [name, setName] = useState<name>({ firstName: '', lastName: '' });
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
@@ -20,6 +26,13 @@ export const StateSample = () => {
       <input type='text' onChange={handleOnChange} />
       <span>Count : {count}</span>
       <button onClick={handleIncreaseCount}>Increase count</button>
+      <br />
+      <p>First Name : {name.firstName}</p>
+      <p>Last Name : {name.lastName}</p>
+      <form>
+        <input type='text' value={name.firstName} onChange={(e) => setName({ ...name, firstName: e.target.value })} />
+        <input type='text' value={name.lastName} onChange={(e) => setName({ ...name, lastName: e.target.value })} />
+      </form>
     </>
   );
 };
