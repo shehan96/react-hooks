623ed02ffe8a54c9be19fdef1b198de6bcbdbbb1
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-06-29 15:38:28 +0530

    Update CssTests.tsx

--- src/CssTests/CssTests.tsx
+++ src/CssTests/CssTests.tsx
@@ -6,14 +6,40 @@ type DogType = {
   status: string;
 };
 
+type DogBoxProps = {
+  image: string | undefined;
+};
+
 const fetchData = async (): Promise<DogType> => {
   const responce = await fetch('https://dog.ceo/api/breeds/image/random');
   const result = await responce.json();
   return result;
 };
 
+const DogBox = ({ image }: DogBoxProps) => {
+  return (
+    <>
+      <div className='card-container'>
+        <div className='card-image'>
+          <img src={image} />
+        </div>
+        <div className='card-content'>
+          <h3>Dog Name</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const CssTests = () => {
   const [duck, setDuck] = useState<DogType>();
+
+  const itemArray: number[] = [0, 1, 2, 3, 4];
 
   useEffect(() => {
     fetchData()
@@ -27,17 +53,14 @@ const CssTests = () => {
 
   return (
     <>
-      <div className='container-box'>
-        <div className='image-holder'>
-          <img className='image' src={duck?.message} />
-        </div>
-        <div className='content-holder'>
-          <h4 className='member-name'>Shehan</h4>
-          <p className='member-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
-          </p>
-        </div>
+      <div className='container'>
+        {itemArray.map((item, index) => {
+          return (
+            <div className='container-item'>
+              <DogBox image={duck?.message} />
+            </div>
+          );
+        })}
       </div>
     </>
   );
